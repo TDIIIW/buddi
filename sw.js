05847ed46b7af96ae60ec25091cf230eebd0fb80
TDIIIW <kieranfranssen@outlook.com>
2026-06-16 22:05:13 +0200

    resposive ness fix

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ const CACHE = 'buddi-v1';
 const ASSETS = ['./', './index.html', './css/app.css', './js/app.js'];
 
 self.addEventListener('install', e => e.waitUntil(
-  caches.open(CACHE).then(c => c.addAll(ASSETS))
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
 ));
 
 self.addEventListener('fetch', e => e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
 ));
